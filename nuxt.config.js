4f1ceca99b38e7fab5835f7ff71b70941c95338d
Emmanuel <rojaz813@gmail.com>
2021-03-02 18:55:38 +0300

    Detailed title

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   target: 'static',
 
   head: {
-    title: 'Emmanuel Karanja',
+    title: 'Emmanuel Karanja - Designer & web developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
