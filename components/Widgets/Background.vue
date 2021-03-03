a905e84c94880590b5da8889e686598ba83865fe
Emmanuel <rojaz813@gmail.com>
2021-03-03 14:48:58 +0300

    Smoothening transition between themes

--- components/Widgets/Background.vue
+++ components/Widgets/Background.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute top-0 right-0 h-full w-full grid grid-cols-2 md:grid-cols-4"
+    class="absolute top-0 right-0 h-full w-full grid grid-cols-2 md:grid-cols-4 transition"
   >
     <span class="bg-gray-50 dark:bg-gray-800"></span>
     <span></span>
